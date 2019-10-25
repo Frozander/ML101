d14f7df4a3bb659c3e8684697f9103ffb221d04f
Frozander <ceylan.mustafabatuhan@gmail.com>
2019-10-25 22:04:32 +0300

    Added relu and relu_der

--- ml101/activations.py
+++ ml101/activations.py
@@ -5,3 +5,9 @@ def sigmoid(x):
 
 def sigmoid_der(x):
     return x / (1 - x)
+
+def relu(x):
+    return x*(x>0) #Python can implicitly convert boolean to number
+
+def relu_der(x):
+    return 1*(x>0)
