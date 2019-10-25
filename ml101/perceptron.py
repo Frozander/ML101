3fef3a27b6ef535677af08a4b9fb2a3c593323c8
Frozander <ceylan.mustafabatuhan@gmail.com>
2019-10-25 21:30:26 +0300

    Added activation/loss func params to perceptron

--- ml101/perceptron.py
+++ ml101/perceptron.py
@@ -3,21 +3,17 @@ from .activations import *
 
 class Perceptron():
     
-    def __init__(self, input_number, threshold=10000, lr=0.01, bias=0):
+    def __init__(self, input_number, threshold=10000, lr=0.01, bias=0, activation_fn=sigmoid, loss_fn=sigmoid_der):
         self.threshold = threshold
         self.lr = lr
         self.weights = np.zeros(input_number + 1)
         self.weights[0] = bias
+        self.activation_fn = activation_fn
+        self.loss_fn = loss_fn
         
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-    
-    def sigmoid_der(self, x):
-        return x / (1 - x)
-    
     def predict(self, inputs):
         total = np.dot(inputs, self.weights[1:]) + self.weights[0]
-        return self.sigmoid(total)
+        return self.activation_fn(total)
         
     def train(self, inputs, labels):        
         for _ in range(self.threshold):
@@ -26,7 +22,7 @@ class Perceptron():
                 prediction = self.predict(value)
                 
                 error = label - prediction
-                loss = self.sigmoid_der(error)
+                loss = self.loss_fn(error)
                 
                 self.weights[1:] += self.lr * loss * value
                 self.weights[0] += self.lr * loss
