35f0a11ccd04f81c6e1befb3873f95445e38e835
Frozander <ceylan.mustafabatuhan@gmail.com>
2019-10-26 23:00:31 +0300

    Added leaky_relu, swish_ and identity

--- ml101/activations.py
+++ ml101/activations.py
@@ -11,3 +11,22 @@ def relu(x):
 
 def relu_der(x):
     return 1*(x>0)
+
+def leaky_relu(x):
+    if x < 0:
+        return 0.01 * x
+    return x
+
+def leaky_relu_der(x):
+    if x < 0:
+        return 0.01
+    return 1
+
+def swish(x):
+    return x / (1 + np.exp(-x))
+
+def swish_der(x):
+    return np.exp(x) * (np.exp(x) + x + 1) / ((np.exp(x) + 1)**2)
+
+def identity(x):
+    return x
