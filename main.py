7d25505b4993bbe799fc47b0dc289eebae109089
Frozander <ceylan.mustafabatuhan@gmail.com>
2019-10-26 10:02:10 +0300

    Cleared trailing white space

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from ml101.perceptron import Perceptron
 
 
 if __name__ == "__main__":
-        
+
     # AND logic gate training
     train_values = np.array([[1,1], [1, 0], [0, 1], [0, 0]])
     train_outputs = np.array([1, 0, 0, 0])
-    
+
     my_perceptron = Perceptron(2)
     my_perceptron.train(train_values, train_outputs)
-    
+
     ## Predition
     test1 = np.array([1, 1])
     test2 = np.array([1, 0])
     test3 = np.array([0, 1])
     test4 = np.array([0, 0])
-    
+
     print("Prediction for {}: {}".format(test1, my_perceptron.predict(test1)))
     print("Prediction for {}: {}".format(test2, my_perceptron.predict(test2)))
     print("Prediction for {}: {}".format(test3, my_perceptron.predict(test3)))
--- ml101/perceptron.py
+++ ml101/perceptron.py
@@ -2,7 +2,7 @@ import numpy as np
 from .activations import *
 
 class Perceptron():
-    
+
     def __init__(self, input_number, threshold=10000, lr=0.01, bias=0, activation_fn=sigmoid, loss_fn=sigmoid_der):
         self.threshold = threshold
         self.lr = lr
@@ -10,19 +10,19 @@ class Perceptron():
         self.weights[0] = bias
         self.activation_fn = activation_fn
         self.loss_fn = loss_fn
-        
+
     def predict(self, inputs):
         total = np.dot(inputs, self.weights[1:]) + self.weights[0]
         return self.activation_fn(total)
-        
-    def train(self, inputs, labels):        
+
+    def train(self, inputs, labels):
         for _ in range(self.threshold):
             for value, label in zip(inputs, labels):
-                
+
                 prediction = self.predict(value)
-                
+
                 error = label - prediction
                 loss = self.loss_fn(error)
-                
+
                 self.weights[1:] += self.lr * loss * value
                 self.weights[0] += self.lr * loss
